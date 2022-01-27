be10eac94a7a9033e4914cb8dfb39ae5fc90e345
totokisaki <sa.mlo.qvxvp@gmail.com>
2022-01-27 23:11:48 +0900

    検査項目の作成、編集、削除の追加

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -8,17 +8,37 @@ class ExamsController < ApplicationController
   end
 
   def create
+    @exam = Exam.new(params.require(:exam).permit(:exam_image, :exam_item, :exam_purpose, :exam_advice))
+      if @exam.save
+        flash[:notice] = "登録しました"
+        redirect_to :exams
+      else
+        render "new"
+      end
   end
 
   def show
+    @exam = Exam.find(params[:id])
   end
 
   def edit
+    @exam = Exam.find(params[:id])
   end
 
   def update
+    @exam = Exam.find(params[:id])
+      if @exam.update(params.require(:exam).permit(:exam_image, :exam_item, :exam_purpose, :exam_advice))
+        flash[:notice] = "情報を更新しました"
+        redirect_to :exams
+      else
+        render "edit"
+      end
   end
 
   def destroy
+    @exam = Exam.find(params[:id])
+      @exam.destroy
+      flash[:notice] = "削除しました"
+      redirect_to :exams
   end
 end
