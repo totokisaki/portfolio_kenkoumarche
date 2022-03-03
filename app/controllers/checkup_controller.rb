78be6375d93e6a161e8ffd539480715a4568f183
totokisaki <sa.mlo.qvxvp@gmail.com>
2022-03-03 20:05:06 +0900

    init

--- app/controllers/checkup_controller.rb
+++ app/controllers/checkup_controller.rb
@@ -6,19 +6,19 @@ class CheckupController < ApplicationController
   def new
     @checkup = Checkup.new
   end
- 
+
   def create
   end
- 
+
   def show
   end
- 
+
   def edit
   end
- 
+
   def update
   end
- 
+  
   def destroy
   end
 end
